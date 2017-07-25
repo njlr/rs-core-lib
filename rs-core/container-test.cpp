3fc5f017ad998caf6c36f57299f8928a57b85853
Ross Smith <rosssmith@me.com>
2017-07-26 04:57:09 +1200

    Add iterators and emplace() to Stack

--- rs-core/container-test.cpp
+++ rs-core/container-test.cpp
@@ -1,5 +1,7 @@
 #include "rs-core/container.hpp"
+#include "rs-core/string.hpp"
 #include "rs-core/unit-test.hpp"
+#include <ostream>
 
 using namespace RS;
 
@@ -72,6 +74,7 @@ namespace {
         TopTail(TopTail&& t): sp(t.sp), ch(t.ch) { t.sp = nullptr; }
         TopTail& operator=(TopTail&& t) { if (&t != this) { term(); sp = t.sp; ch = t.ch; t.sp = nullptr; } return *this; }
         char get() const noexcept { return ch; }
+        friend std::ostream& operator<<(std::ostream& out, const TopTail& t) { return out << t.ch; }
     private:
         U8string* sp;
         char ch;
@@ -86,20 +89,25 @@ namespace {
         U8string s;
 
         TEST(st.empty());
-        TRY(st.push(TopTail(s, 'a')));
+        TEST_EQUAL(to_str(st), "[]");
+        TRY(st.emplace(s, 'a'));
         TEST_EQUAL(st.size(), 1);
         TEST_EQUAL(st.top().get(), 'a');
+        TEST_EQUAL(to_str(st), "[a]");
         TEST_EQUAL(s, "+a");
-        TRY(st.push(TopTail(s, 'b')));
+        TRY(st.emplace(s, 'b'));
         TEST_EQUAL(st.size(), 2);
         TEST_EQUAL(st.top().get(), 'b');
+        TEST_EQUAL(to_str(st), "[a,b]");
         TEST_EQUAL(s, "+a+b");
-        TRY(st.push(TopTail(s, 'c')));
+        TRY(st.emplace(s, 'c'));
         TEST_EQUAL(st.size(), 3);
         TEST_EQUAL(st.top().get(), 'c');
+        TEST_EQUAL(to_str(st), "[a,b,c]");
         TEST_EQUAL(s, "+a+b+c");
         TRY(st.clear());
         TEST(st.empty());
+        TEST_EQUAL(to_str(st), "[]");
         TEST_EQUAL(s, "+a+b+c-c-b-a");
 
     }
